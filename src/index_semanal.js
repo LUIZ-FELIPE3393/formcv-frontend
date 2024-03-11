4c2acc2bc6618de044a3d5be6b8d8c289f7975d8
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-03-11 16:59:30 -0400

    Melhoria no visual e validação de datas
    Agora a inserção de datas para criação de relatórios passa primeiramente por uma validação para verificar se o formato e valor do dia, mês e ano estão corretos

--- src/index_semanal.js
+++ src/index_semanal.js
@@ -1,7 +1,58 @@
 const content = document.getElementById('rel_list');
 const button_logDate = document.getElementById("button-log_date");
+const errDisplay = document.getElementById('input-log_date-err');
 
 let test_id = 0;
+
+function verifyDate(day, month, year)
+{
+    if (day < 1 || month < 1 || year < 1965 || year > 9999)
+        return false;
+
+    if (month === 4 || month === 6 || month === 9 || month === 11)
+        if (day > 30)
+            return false;
+    if (month === 2) {
+        if ((year % 100 === 0 && year % 400 === 0) || (year % 4 === 0)) {
+            if (day > 29)
+                return false;
+        }
+        else if (day > 28)
+            return false;
+    }
+    return true;
+}
+
+function convertToDateObj(date_data) {
+    //Formato da data DD/MM/AAAA
+    let dateArr = date_data.split('/');
+
+    if(dateArr.length < 3)
+        return 0;
+
+    if(!verifyDate(parseInt(dateArr[0]), parseInt(dateArr[1]), parseInt(dateArr[2])))
+        return 0;
+
+    console.log(dateArr);
+
+    let dateCheck = dateArr[2]+"/"+dateArr[1]+"/"+dateArr[0];
+    
+    let dateObj = new Date(dateCheck);
+
+    console.log(dateObj);
+    console.log(!isNaN(dateObj));
+
+    if (isNaN(dateObj))
+    {
+        return 0;
+    }
+
+    let dateFormated =  ("00" + dateArr[0]).slice(-2) + "/" +
+                        ("00" + dateArr[1]).slice(-2) + "/" +
+                        ("0000" + dateArr[2]).slice(-4);
+
+    return dateFormated;
+}
 
 button_logDate.addEventListener('click', function() {
     let input_logDate = document.getElementById("input-log_date");
@@ -14,10 +65,20 @@ function remove_relatorio(tuple_id) {
     content.removeChild(document.getElementById('tuple-data-'+tuple_id));
 }
 
-function new_relatorio(date_data) {   
-    const tuple = document.createElement("div");
-    let idAttr = "tuple-data-" + date_data;
+function convertDateFormat(dateObj) {
+    return (dateObj.getDay()) + "/" + (dateObj.getMonth()) + "/" + dateObj.getFullYear();
+}
 
+function new_relatorio(dateStr) {   
+    let dateFormated = convertToDateObj(dateStr);
+    errDisplay.innerText = "";
+    
+    if(dateFormated === 0) {
+        errDisplay.innerText = "ERRO - Data inválida!";
+        return;
+    }
+
+    const tuple = document.createElement("div");
 
     tuple.setAttribute("id", "tuple-data-" + test_id);
     tuple.setAttribute("class", "row align-items-start tuple-data");
@@ -35,16 +96,17 @@ function new_relatorio(date_data) {
     <div class="col-1 tuple-heading-text column" style="text-align: center;">
         <!-- Aqui o usuário é enviado para a tela de edição do relatório semanal -->
         <a href="rel_semanal.html">
-            <img src="../res/lupa.svg" style="height: 20px;"></img>
+            <img src="../res/lupa.svg" style="height: 25px;"></img>
         </a>
     </div>
     <div class="col-1 tuple-heading-text column" style="text-align: center;">
-            <img id="button-remove-:tuple-id:" src="../res/lixo.svg" style="height: 27px; cursor: pointer;"></img>
+            <img id="button-remove-:tuple-id:" src="../res/lixo.svg" style="height: 25px; cursor: pointer;"></img>
         <!-- Deleção de relatório semanal -->
     </div>
-        `;
+    `;
     
-    tupleContent = tupleContent.replace(":date:", date_data);
+    
+    tupleContent = tupleContent.replace(":date:", dateFormated);
     tupleContent = tupleContent.replace(":tuple-id:", test_id);
 
     tuple.innerHTML = tupleContent;
